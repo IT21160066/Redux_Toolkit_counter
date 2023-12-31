6b2830cd1da76bd217a8bb5f648e5bdb41893cf7
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-31 11:49:26 +0530

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,48 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const reducerFunction = (state = { counter: 0 }, action) => {
-  //synchronus function
-  //not mutate the original state
-  //always copy of the state, main state should be updated
+// const reducerFunction = (state = { counter: 0 }, action) => {
+//   //synchronus function
+//   //not mutate the original state
+//   //always copy of the state, main state should be updated
 
-  if (action.type === "INC") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "DEC") {
-    return { counter: state.counter - 1 };
-  }
-  if (action.type === "ADD") {
-    return { counter: state.counter + action.payload };
-  }
-  return state;
-};
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   if (action.type === "ADD") {
+//     return { counter: state.counter + action.payload };
+//   }
+//   return state;
+// };
 
-const store = createStore(reducerFunction);
+// const store = createStore(reducerFunction);
 
-export default store;
+// export default store;
+
+//toolkit
+
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    addBy(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
+
+export const actions = counterSlice.actions;
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+export default store; //24.38
